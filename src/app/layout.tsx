86917a6c8509a9d62ccccb0d53f33148ab148695
huynhdoanteq <huynhdoan@teqnological.asia>
2023-06-12 13:36:47 +0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div id="map">
+        {children}
+        </div>
+      </body>
     </html>
   )
 }
